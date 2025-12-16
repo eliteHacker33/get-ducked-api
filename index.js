22019bceeff8b83b0e8e6e7012bf9f40afaf3189
eliteHacker33 <anambypamby@gmail.com>
2025-12-15 21:06:47 -0600

    ensure that dotenv is only used in local development

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 // Import the framework and instantiate it
-import Fastify from 'fastify';
-import qrCode from './routes/qrCode/index.js';
+import Fastify from "fastify";
+
+if (process.env.NODE_ENV !== "production" && process.env.NODE_ENV !== "test") {
+  const { config } = await import("dotenv");
+  config();
+}
 const fastify = Fastify({
   logger: true,
 });
 
 // Declare a route
-fastify.get('/', async function handler(request, reply) {
-  return { hello: 'world' };
+fastify.get("/", async function handler(request, reply) {
+  return { hello: "world" };
 });
 
-await fastify.register(import('./routes/qrCode/index.js'), { prefix: '/qrCode' });
+await fastify.register(import("./routes/qrCode/index.js"), {
+  prefix: "/qrCode",
+});
 
 // Run the server!
 try {
